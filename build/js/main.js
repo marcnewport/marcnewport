f7b0ca1bbe27e2923ab9f77a254886f89fe3bff8
Marc Newport <marcnewport@gmail.com>
2015-10-06 23:14:48 +1100

    static build progress

--- build/js/main.js
+++ build/js/main.js
@@ -8,17 +8,23 @@
         winWidth = 0,
         $toggle = $('#navigation-toggle'),
         $navigation = $('#navigation'),
-        $home = $('#home');
+        $home = $('#home'),
+        $homeContainer = $home.find('.container'),
+        $awards = $('#awards').find('.award-item');
 
     //add some listeners to the window
     $window.bind('resize', windowResizeHandler)
-    .bind('scroll', windowScrollHandler)
-    //call the resize listener immediately
-    .trigger('resize');
+      .bind('scroll', windowScrollHandler)
+      //call the resize listener immediately
+      .trigger('resize');
 
+    //toggle the nav
     $toggle.on('click', function() {
-      $navigation.css({ left:0 })
+      $navigation.css({ left:0 });
     });
+
+    //turn on tooltips
+    $('[data-toggle="tooltip"]').tooltip();
 
 
 
@@ -26,17 +32,36 @@
      * Handles window resize
      */
     function windowResizeHandler() {
-
+      //get window dimensions
       winWidth = $window.width();
       winHeight = $window.height();
 
-      var $container = $home.find('.container'),
-          margin = (winHeight - $container.height()) - 30;
+      var margin = winHeight - $homeContainer.height(),
+          awardHeight = 0;
 
-      //TODO only if the content is less than the window size
+      //hide the nav
       $navigation.css({ left:'-'+ winWidth +'px' });
-      $('section').height(winHeight);
-      $container.css({ marginTop:margin });
+
+      //if the section's height is smaller than the window,
+      //make it the height of the window
+      // $('section').each(function() {
+      //   var $this = $(this);
+      //
+      //   if ($this.height() < winHeight) {
+      //     $this.height(winHeight);
+      //   }
+      // });
+
+      //place MARC NEWPORT text near bottom of container
+      $homeContainer.css({ marginTop:margin });
+      windowScrollHandler();
+
+      //set award height to tallest
+      $awards.each(function() {
+        awardHeight = Math.max(awardHeight, $(this).height());
+      });
+
+      $awards.height(awardHeight);
     }
 
 
@@ -47,15 +72,18 @@
     function windowScrollHandler() {
 
       var position = $window.scrollTop(),
-          bgParallax = 0.3,
+          bgParallax = 0.4,
           bgTop = Math.round(position * bgParallax),
           fgParallax = 0.6,
           fgTop = Math.round(position * fgParallax),
           percent = (position / (winHeight - 100)) * 100,
           opacity = (100 - percent) / 100;
 
-      $('#home').css({ backgroundPosition:'50% '+ bgTop +'px' })
-      .find('.row').eq(0).css({ marginTop:fgTop +'px', opacity:opacity });
+      opacity = opacity.toFixed(5);
+      if (opacity < 0) opacity = 0;
+
+      $home.css({ backgroundPosition:'50% '+ bgTop +'px' })
+        .find('.row').eq(0).css({ position:'relative', top:fgTop +'px', opacity:opacity });
     }
   });
 }(jQuery));
