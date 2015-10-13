dfe685d21608d80fb04bcaaacdb5cccb930631c0
Marc Newport <marcnewport@gmail.com>
2015-10-14 08:41:33 +1100

    built projects slider. saving progress.

--- build/js/main.js
+++ build/js/main.js
@@ -6,11 +6,15 @@
     var $window = $(window),
         winHeight = 0,
         winWidth = 0,
+        containerWidth = 0,
         $toggle = $('#navigation-toggle'),
         $navigation = $('#navigation'),
         $home = $('#home'),
         $homeContainer = $home.find('.container'),
-        $awards = $('#awards').find('.award-item');
+        $awards = $('#awards').find('.award-item'),
+        $projectContainer = $('#project-container'),
+        $projects = $projectContainer.find('.project-item'),
+        $contacts = $('#contact').find('.contact-list');
 
     //add some listeners to the window
     $window.bind('resize', windowResizeHandler)
@@ -24,45 +28,9 @@
     });
 
     //turn on tooltips
-    $('[data-toggle="tooltip"]').tooltip();
+    //$('[data-toggle="tooltip"]').tooltip();
 
-
-
-    /**
-     * Handles window resize
-     */
-    function windowResizeHandler() {
-      //get window dimensions
-      winWidth = $window.width();
-      winHeight = $window.height();
-
-      var margin = winHeight - $homeContainer.height(),
-          awardHeight = 0;
-
-      //hide the nav
-      $navigation.css({ left:'-'+ winWidth +'px' });
-
-      //if the section's height is smaller than the window,
-      //make it the height of the window
-      // $('section').each(function() {
-      //   var $this = $(this);
-      //
-      //   if ($this.height() < winHeight) {
-      //     $this.height(winHeight);
-      //   }
-      // });
-
-      //place MARC NEWPORT text near bottom of container
-      $homeContainer.css({ marginTop:margin });
-      windowScrollHandler();
-
-      //set award height to tallest
-      $awards.each(function() {
-        awardHeight = Math.max(awardHeight, $(this).height());
-      });
-
-      $awards.height(awardHeight);
-    }
+    setupProjects();
 
 
 
@@ -85,5 +53,147 @@
       $home.css({ backgroundPosition:'50% '+ bgTop +'px' })
         .find('.row').eq(0).css({ position:'relative', top:fgTop +'px', opacity:opacity });
     }
+
+
+
+    /**
+     * Handles window resize
+     */
+    function windowResizeHandler() {
+      //get window dimensions
+      winWidth = $window.width();
+      winHeight = $window.height();
+      containerWidth = $homeContainer.width();
+
+      //if the section's height is smaller than the window,
+      //make it the height of the window
+      // $('section').each(function() {
+      //   var $this = $(this);
+      //
+      //   if ($this.height() < winHeight) {
+      //     $this.height(winHeight);
+      //   }
+      // });
+
+      // conatctSize = $contacts.find('div').eq(0).width();
+      // $contacts.find('a').css({
+      //   width: conatctSize,
+      //   height: conatctSize
+      // })
+      // .find('i').css({
+      //   lineHeight: conatctSize +'px',
+      //   fontSize: (conatctSize * 0.5) +'px'
+      // });
+
+
+
+      //hide the nav
+      $navigation.css({ left:'-'+ winWidth +'px' });
+
+      resizeHome();
+      resizeAwards();
+      resizeProjects();
+
+      //call the scroll handler to update home parralax
+      windowScrollHandler();
+    }
+
+
+
+    /**
+     * Resize the home section
+     */
+    function resizeHome() {
+
+      var margin = winHeight - $homeContainer.height();
+      //place MARC NEWPORT text near bottom of container
+      $homeContainer.css({ marginTop:margin });
+    }
+
+
+
+    /**
+     * Resize the awards section
+     */
+    function resizeAwards() {
+
+      var awardHeight = 0;
+
+      //set award height to tallest
+      $awards.each(function() {
+        awardHeight = Math.max(awardHeight, $(this).height());
+      });
+
+      $awards.height(awardHeight);
+    }
+
+
+
+    /**
+     * Resize the awards section
+     */
+    function resizeProjects() {
+
+      // var count = $projects.length;
+      //
+      // $projects.width(containerWidth);
+      // $projectContainer.width(containerWidth * (count + 300));
+
+      $projectContainer.height($projects.height() + 50);
+
+      $projects.each(function(i) {
+        //this.style.left = containerWidth * i + 30;
+        $(this).css({
+          width: containerWidth +'px',
+          left: Number((containerWidth + 50) * i) +'px'
+        });
+      });
+    }
+
+
+
+    function setupProjects() {
+
+      var $left = $('#slide-left'),
+          $right = $('#slide-right'),
+          index = 0,
+          count = $projects.length;
+
+      $left.bind('click', function(e) {
+        if (index > 0) {
+          $projects.css({
+            left: '+='+ Number(containerWidth + 50) +'px'
+          });
+
+          $right.removeClass('disabled');
+          index--;
+        }
+
+        if (index === 0) {
+          $left.addClass('disabled');
+        }
+
+        e.preventDefault();
+      });
+
+      $right.bind('click', function(e) {
+        if (index < count - 1) {
+          $projects.css({
+            left: '-='+ Number(containerWidth + 50) +'px'
+          });
+
+          $left.removeClass('disabled');
+          index++;
+        }
+
+        if (index === count - 1) {
+          $right.addClass('disabled');
+        }
+
+        e.preventDefault();
+      });
+
+    }
+
   });
 }(jQuery));
