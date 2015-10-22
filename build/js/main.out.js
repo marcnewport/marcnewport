335991212ff826b95a23f71daf37118f200cd4b2
Marc Newport <marcnewport@gmail.com>
2015-10-22 22:19:49 +1100

    added npm as a build tool - using jade, scss, browserify etc.

--- build/js/main.out.js
+++
@@ -1,6 +0,0 @@
-/*
-This file has been generated with SiteFlow (http://siteflow.witiz.com).
-To remove this comment, please support us and upgrade to SiteFlow Pro.
-*/
-!function(n){n(document).ready(function(){var i=n(window);i.bind("resize",function(){var i=window.innerHeight,r=n("#home"),o=r.find(".container"),e=(i-o.height())/2;r.height(i);o.css({marginTop:e})}).bind("scroll",function(){var r=i.scrollTop(),o=Math.round(r/4);n(".img-bg").css({marginTop:o+"px"})}).trigger("resize")})}(jQuery);
-
